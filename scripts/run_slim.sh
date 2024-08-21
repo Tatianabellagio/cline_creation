debda9d0571e0087eab976e0a58d67e246f86bf4
Tatiana Bellagio <tatianabellagio@gmail.com>
2024-08-21 14:46:03 -0700

    simplifying variables in teh sim

--- scripts/run_slim.sh
+++ scripts/run_slim.sh
@@ -1,5 +1,4 @@
-#!/usr/local/bin/bash
- 
+
 ## This script requires jq to be installed to read JSON files
 ## Get the hash value
 
@@ -19,7 +18,7 @@ subpop_number=$(jq -r '.subpop_number' "$parameter_space")
 migration_rate=$(jq -r '.migration_rate' "$parameter_space")
 BURNIN_1=$(jq -r '.BURNIN_1' "$parameter_space")
 BURNIN_2=$(jq -r '.BURNIN_2' "$parameter_space")
-last_gen_a_cg=$(jq -r '.last_gen_a_cg' "$parameter_space")
+cg_duration=$(jq -r '.cg_duration' "$parameter_space")
 initial_sel_strength=$(jq -r '.initial_sel_strength' "$parameter_space")
 final_sel_strength=$(jq -r '.final_sel_strength' "$parameter_space")
 
@@ -27,7 +26,6 @@ final_sel_strength=$(jq -r '.final_sel_strength' "$parameter_space")
 common_garden_number=$(jq -r '.common_garden_number' "$parameter_space")
 common_garden_migrants=$(jq -r '.common_garden_migrants' "$parameter_space")
 sel_strength_CG=$(jq -r '.sel_strength_CG' "$parameter_space")
-last_gen_a_cg=$(jq -r '.last_gen_a_cg' "$parameter_space")
 COMMON_GARDEN_CYCLE=$(jq -r '.COMMON_GARDEN_CYCLE' "$parameter_space")
 
 
@@ -45,13 +43,12 @@ slim \
     -d "migration_rate=$migration_rate" \
     -d "BURNIN_1=$BURNIN_1" \
     -d "BURNIN_2=$BURNIN_2" \
-    -d "last_gen_a_cg=$last_gen_a_cg" \
+    -d "cg_duration=$cg_duration" \
     -d "initial_sel_strength=$initial_sel_strength" \
     -d "final_sel_strength=$final_sel_strength" \
     -d "common_garden_number=$common_garden_number" \
     -d "common_garden_migrants=$common_garden_migrants" \
     -d "sel_strength_CG=$sel_strength_CG" \
-    -d "last_gen_a_cg=$last_gen_a_cg" \
     -d "COMMON_GARDEN_CYCLE=$COMMON_GARDEN_CYCLE" \
     scripts/cline_creation.slim
 
