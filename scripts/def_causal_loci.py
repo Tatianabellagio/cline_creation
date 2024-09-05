c70841e1d018aa5797d8fff51217e1a667855951
Tatiana Bellagio <tatianabellagio@gmail.com>
2024-09-04 17:21:37 -0700

    space in between casual loi

--- scripts/def_causal_loci.py
+++ scripts/def_causal_loci.py
@@ -12,8 +12,18 @@ with open(parameter_space, 'r') as f:
 number_of_causal_loci = parameters['causal_loci']
 genome_length = parameters['genome_length']
 
-causal_loci = [random.randint(0, genome_length) for _ in range(number_of_causal_loci)]
-causal_loci.sort()
+min_distance = 2  # Minimum distance between loci if not it will create problems in slim 
+
+# Generate causal loci ensuring they are not consecutive
+causal_loci = set()
+while len(causal_loci) < number_of_causal_loci:
+    new_locus = random.randint(0, 500000)
+    # Ensure the new locus doesn't violate the minimum distance rule
+    if all(abs(new_locus - locus) >= min_distance for locus in causal_loci):
+        causal_loci.add(new_locus)
+
+# Sort the loci
+causal_loci = sorted(causal_loci)
 
 with open(causal_loci_output, 'w') as f:
     for loci in causal_loci:
